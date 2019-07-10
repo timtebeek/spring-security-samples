c15c404e8a9915304b581e3628ab270f84e1c6ee
Tim te Beek <tim.te.beek@jdriven.com>
2019-07-10 14:15:43 +0200

    Write Audit Spring Data Entities blogpost

--- audit-spring-data-entities/src/main/java/com/jdriven/SpringSecurityApplication.java
+++ audit-spring-data-entities/src/main/java/com/jdriven/SpringSecurityApplication.java
@@ -2,10 +2,8 @@ package com.jdriven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
--- audit-spring-data-entities/src/main/java/com/jdriven/repo/BlogpostRepository.java
+++ audit-spring-data-entities/src/main/java/com/jdriven/repo/BlogpostRepository.java
@@ -1,13 +1,9 @@
 package com.jdriven.repo;
 
-import java.util.List;
-
 import org.springframework.data.repository.Repository;
 
 public interface BlogpostRepository extends Repository<Blogpost, String> {
 
 	Blogpost save(Blogpost entity);
-
-	List<Blogpost> findByTitleAndPublishedIsTrue(String author);
 
 }
