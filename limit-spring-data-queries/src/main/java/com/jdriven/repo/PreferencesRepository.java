ad37b3d135904719f325991cc30ea01978ee43c0
Tim te Beek <tim.te.beek@jdriven.com>
2019-07-29 07:04:04 +0200

    Demonstrate PostAuthorize

--- limit-spring-data-queries/src/main/java/com/jdriven/repo/PreferencesRepository.java
+++ limit-spring-data-queries/src/main/java/com/jdriven/repo/PreferencesRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-public interface PreferencesRepository extends Repository<Preferences, String> {
-
-	@Query("select p from #{#entityName} p where p.user.name = ?#{ authentication.name }")
-	Optional<Preferences> findOne();
+public interface PreferencesRepository extends Repository<Preferences, Long> {
 
 	@PreAuthorize("#entity.user.name == authentication.name")
 	Preferences save(Preferences entity);
+
+	@PostAuthorize("returnObject != null and returnObject.user.name = authentication.name")
+	Subset findOne(Long id);
+
+	@Query("select p from #{#entityName} p where p.user.name = ?#{ authentication.name }")
+	Optional<Preferences> findOne();
 
 }
