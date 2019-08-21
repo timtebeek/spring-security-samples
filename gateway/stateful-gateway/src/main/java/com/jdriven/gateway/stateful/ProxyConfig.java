55b1b9ffbae543b4eca419f9aac578f1c78671a3
Robbert van Waveren <robbert.van.waveren@vialis.nl>
2019-08-22 00:11:26 +0200

    fully working with integration test

--- gateway/stateful-gateway/src/main/java/com/jdriven/gateway/stateful/ProxyConfig.java
+++ gateway/stateful-gateway/src/main/java/com/jdriven/gateway/stateful/ProxyConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
@@ -22,6 +23,7 @@ import java.util.function.Consumer;
 import static org.springframework.web.reactive.function.BodyExtractors.toDataBuffers;
 import static org.springframework.web.reactive.function.BodyInserters.fromDataBuffers;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -31,6 +33,7 @@ public class ProxyConfig {
 
 	private static final String API_PREFIX = "/api/";
 	private static final String CATCH_ALL_SUFFIX = "/**";
+	private static final String SERVICE_PARAM = "{service}";
 
 	private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
 	private final GatewayConfig config;
@@ -47,15 +50,30 @@ public class ProxyConfig {
 
 	@Bean
 	public RouterFunction<ServerResponse> proxy(WebClient webClient) {
-		return route(GET(API_PREFIX + "{service}" + CATCH_ALL_SUFFIX), request -> authorizedClient(request)
+		return route(path(API_PREFIX + SERVICE_PARAM + CATCH_ALL_SUFFIX), request -> authorizedClient(request)
 			.map(attr -> webClient.method(request.method())
 				.uri(toBackendUri(request))
+				.headers(cleanedHeaders(request.headers().asHttpHeaders()))
 				.body(fromDataBuffers(request.exchange().getRequest().getBody()))
 				.attributes(attr))
 			.flatMap(WebClient.RequestHeadersSpec::exchange)
 			.flatMap(response -> ServerResponse.status(response.statusCode())
-				.headers(headers -> headers.putAll(response.headers().asHttpHeaders()))
+				.headers(cleanedHeaders(response.headers().asHttpHeaders()))
 				.body(fromDataBuffers(response.body(toDataBuffers())))));
+	}
+
+	private Consumer<HttpHeaders> cleanedHeaders(final HttpHeaders httpHeaders) {
+		return headers -> {
+			headers.putAll(httpHeaders);
+			headers.remove(HttpHeaders.COOKIE);
+			headers.remove(HttpHeaders.SET_COOKIE);
+			headers.remove(HttpHeaders.SET_COOKIE2);
+			headers.remove(HttpHeaders.HOST);
+			headers.remove(HttpHeaders.TRANSFER_ENCODING);
+			headers.remove(HttpHeaders.CONNECTION);
+			headers.remove("keep-alive");
+			// etc
+		};
 	}
 
 	private String toBackendUri(final ServerRequest request) {
