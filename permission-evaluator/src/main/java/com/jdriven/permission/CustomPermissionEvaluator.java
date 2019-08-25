a3603928ac9ec7384f53566815c25eaa18b4711a
Tim te Beek <tim.te.beek@jdriven.com>
2019-08-25 15:13:35 +0200

    Refactor to signify how to support different types

---
+++ permission-evaluator/src/main/java/com/jdriven/permission/CustomPermissionEvaluator.java
@@ -0,0 +1,62 @@
+package com.jdriven.permission;
+
+import java.io.Serializable;
+
+import com.jdriven.model.Spreadsheet;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CustomPermissionEvaluator implements PermissionEvaluator {
+
+	private final PermissionStore store;
+
+	@Override
+	public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+		User principal = (User) authentication.getPrincipal();
+
+		if (targetDomainObject instanceof Spreadsheet) {
+			return hasSpreadsheetPermission(principal, (Spreadsheet) targetDomainObject, permission);
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
+			Object permission) {
+		User principal = (User) authentication.getPrincipal();
+
+		if (targetType.equals(Spreadsheet.class.getName())) {
+			return hasSpreadsheetPermission(principal, targetId, permission);
+		}
+
+		return false;
+	}
+
+	private boolean hasSpreadsheetPermission(User principal, Spreadsheet spreadsheet, Object permission) {
+		boolean hasPermission = store.getPermissions().stream().anyMatch(p -> p.getUser().equals(principal)
+				&& p.getSpreadsheet().equals(spreadsheet)
+				&& p.getLevel().equals(permission));
+		if (!hasPermission) {
+			log.warn("Denying {} {} access to {}", principal, permission, spreadsheet);
+		}
+		return hasPermission;
+	}
+
+	private boolean hasSpreadsheetPermission(User principal, Serializable targetId, Object permission) {
+		boolean hasPermission = store.getPermissions().stream().anyMatch(p -> p.getUser().equals(principal)
+				&& p.getSpreadsheet().getId().equals(targetId)
+				&& p.getLevel().equals(permission));
+		if (!hasPermission) {
+			log.warn("Denying {} {} access to Spreadsheet with id {}", principal, permission, targetId);
+		}
+		return hasPermission;
+	}
+}
--- permission-evaluator/src/main/java/com/jdriven/service/SpreadsheetService.java
+++ permission-evaluator/src/main/java/com/jdriven/service/SpreadsheetService.java
@@ -3,7 +3,6 @@ package com.jdriven.service;
 import com.jdriven.model.Spreadsheet;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,27 +11,22 @@ public class SpreadsheetService {
 
 	@PreAuthorize("hasPermission(#spreadsheet, 'READ')")
 	public void read(Spreadsheet spreadsheet) {
-		log.info("Reading {} as {}", spreadsheet,
-				SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		log.info("Reading {}", spreadsheet);
 	}
 
 	@PreAuthorize("hasPermission(#id, 'com.jdriven.model.Spreadsheet', 'READ')")
 	public void readById(Long id) {
-		log.info("Reading Spreadsheet id {} as {}", id,
-				SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-
+		log.info("Reading Spreadsheet id {} ", id);
 	}
 
 	@PreAuthorize("hasPermission(#spreadsheet, 'WRITE')")
 	public void write(Spreadsheet spreadsheet) {
-		log.info("Writing {} as {}", spreadsheet,
-				SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		log.info("Writing {} ", spreadsheet);
 	}
 
 	@PreAuthorize("hasPermission(#id, 'com.jdriven.model.Spreadsheet', 'WRITE')")
 	public void writeById(Long id) {
-		log.info("Writing Spreadsheet id {} as {}", id,
-				SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		log.info("Writing Spreadsheet id {} ", id);
 	}
 
 }
