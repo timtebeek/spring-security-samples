ba7d2e9d5cfa4f8e8777420cf4f92e8106ecb929
Tim te Beek <tim.te.beek@jdriven.com>
2019-10-19 18:18:53 +0200

    Add tests using JwtRequestPostProcessor

---
+++ spring-cloud-gateway-oidc-tokenrelay/mock-flights-service/src/test/java/com/jdriven/flights/FlightsApplicationTest.java
@@ -0,0 +1,29 @@
+package com.jdriven.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FlightsApplicationTest {
+
+	@Autowired
+	private MockMvc mockmvc;
+
+	@Test
+	void testGetIndex() throws Exception {
+		mockmvc.perform(get("/").with(jwt(builder -> builder.subject("Subject A"))))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Welcome <a href=\"/whoami\"><span>Subject A</span></a>!")));
+	}
+
+}
