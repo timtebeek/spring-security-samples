993218cc5c93bdb0c421bfbe9fbeee4b77524789
Tim te Beek <tim.te.beek@jdriven.com>
2019-10-19 18:07:24 +0200

    Convert any keycloak realm_access.roles to ROLE_

--- spring-cloud-gateway-oidc-tokenrelay/mock-flights-service/src/main/java/com/jdriven/flights/SecurityConfig.java
+++ spring-cloud-gateway-oidc-tokenrelay/mock-flights-service/src/main/java/com/jdriven/flights/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.jdriven.flights;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +13,10 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
@@ -24,7 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().sameOrigin();
 		http.authorizeRequests().anyRequest().authenticated();
-		http.oauth2ResourceServer().jwt();
+		http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+		return jwtAuthenticationConverter;
 	}
 
 	@Bean
@@ -39,10 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public SpringSecurityDialect springSecurityDialect() {
 		return new SpringSecurityDialect();
 	}
+
 }
 
-//As per: https://docs.spring.io/spring-security/site/docs/5.2.x/reference/html5/#oauth2resourceserver-jwt-claimsetmapping-rename
+// As per: https://docs.spring.io/spring-security/site/docs/5.2.x/reference/html5/#oauth2resourceserver-jwt-claimsetmapping-rename
 class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
+
 	private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
 
 	@Override
@@ -52,4 +63,19 @@ class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<Stri
 		convertedClaims.put("sub", username);
 		return convertedClaims;
 	}
+
+}
+
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Collection<GrantedAuthority> convert(final Jwt jwt) {
+		final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+		return ((List<String>) realmAccess.get("roles")).stream()
+				.map(roleName -> "ROLE_" + roleName)
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
 }
--- spring-cloud-gateway-oidc-tokenrelay/mock-hotels-service/src/main/java/com/jdriven/hotels/SecurityConfig.java
+++ spring-cloud-gateway-oidc-tokenrelay/mock-hotels-service/src/main/java/com/jdriven/hotels/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.jdriven.hotels;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +13,10 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
@@ -25,7 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().sameOrigin();
 		http.authorizeRequests().anyRequest().authenticated();
-		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+		http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+		return jwtAuthenticationConverter;
 	}
 
 	@Bean
@@ -40,10 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public SpringSecurityDialect springSecurityDialect() {
 		return new SpringSecurityDialect();
 	}
+
 }
 
 // As per: https://docs.spring.io/spring-security/site/docs/5.2.x/reference/html5/#oauth2resourceserver-jwt-claimsetmapping-rename
 class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
+
 	private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
 
 	@Override
@@ -53,4 +63,19 @@ class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<Stri
 		convertedClaims.put("sub", username);
 		return convertedClaims;
 	}
+
+}
+
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Collection<GrantedAuthority> convert(final Jwt jwt) {
+		final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+		return ((List<String>) realmAccess.get("roles")).stream()
+				.map(roleName -> "ROLE_" + roleName)
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
 }
