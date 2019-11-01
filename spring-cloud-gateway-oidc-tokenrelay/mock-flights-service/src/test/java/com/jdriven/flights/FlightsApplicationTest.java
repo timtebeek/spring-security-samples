d851e685a7810c7ab786dcffdad3878102582e41
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-01 16:02:50 +0100

    Mock out the JwtDecoder bean to enable FlightsApplicationTest

--- spring-cloud-gateway-oidc-tokenrelay/mock-flights-service/src/test/java/com/jdriven/flights/FlightsApplicationTest.java
+++ spring-cloud-gateway-oidc-tokenrelay/mock-flights-service/src/test/java/com/jdriven/flights/FlightsApplicationTest.java
@@ -1,10 +1,11 @@
 package com.jdriven.flights;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -15,8 +16,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled("requires keycloak to start")
 class FlightsApplicationTest {
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Autowired
 	private MockMvc mockmvc;
