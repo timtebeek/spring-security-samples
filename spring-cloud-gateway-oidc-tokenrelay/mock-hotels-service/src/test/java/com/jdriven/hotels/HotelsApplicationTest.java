d415a64008c6563456fd3fd1610d1a34202a3c7f
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-01 15:41:44 +0100

    Enable HotelsApplicationTest by serving recorded OpenID connect response

--- spring-cloud-gateway-oidc-tokenrelay/mock-hotels-service/src/test/java/com/jdriven/hotels/HotelsApplicationTest.java
+++ spring-cloud-gateway-oidc-tokenrelay/mock-hotels-service/src/test/java/com/jdriven/hotels/HotelsApplicationTest.java
@@ -1,10 +1,10 @@
 package com.jdriven.hotels;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled("requires keycloak to start")
+@AutoConfigureWireMock(port = 8090)
 class HotelsApplicationTest {
 
 	@Autowired
