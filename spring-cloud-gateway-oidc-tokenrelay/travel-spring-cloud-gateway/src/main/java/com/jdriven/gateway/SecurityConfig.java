88234f6d42edb26432a2f65508d61ba2c9ca4941
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-04 17:16:08 +0100

    Also logout at the OpenID Connect provider

--- spring-cloud-gateway-oidc-tokenrelay/travel-spring-cloud-gateway/src/main/java/com/jdriven/gateway/SecurityConfig.java
+++ spring-cloud-gateway-oidc-tokenrelay/travel-spring-cloud-gateway/src/main/java/com/jdriven/gateway/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.jdriven.gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
 
@@ -10,9 +12,13 @@ import org.springframework.security.web.server.header.XFrameOptionsServerHttpHea
 public class SecurityConfig {
 
 	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+			ReactiveClientRegistrationRepository clientRegistrationRepository) throws Exception {
 		// Authenticate through configured OpenID Provider
 		http.oauth2Login();
+		// Also logout at the OpenID Connect provider
+		http.logout(logout -> logout.logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(
+				clientRegistrationRepository)));
 		// Require authentication for all requests
 		http.authorizeExchange().anyExchange().authenticated();
 		// Allow showing /home within a frame
