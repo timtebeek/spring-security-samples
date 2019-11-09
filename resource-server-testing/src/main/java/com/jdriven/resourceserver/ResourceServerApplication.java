fe591fca2c09ed2172a4be99e29d94ca1c10cea1
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-09 14:57:31 +0100

    Add placeholder project with tests but without Spring Security

---
+++ resource-server-testing/src/main/java/com/jdriven/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,11 @@
+package com.jdriven.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+}
