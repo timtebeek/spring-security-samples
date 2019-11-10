04887c0dcfb0242f1ee180d05ce1ce34a65f8078
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-10 16:27:46 +0100

    Add security expressions to LeaveRequestService

--- adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/LeaveRequestService.java
+++ adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/LeaveRequestService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.annotation.security.RolesAllowed;
+
 import com.jdriven.leaverequest.LeaveRequest.Status;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +19,7 @@ public class LeaveRequestService {
 
 	private final LeaveRequestRepository repo;
 
+	@PreAuthorize("#employee == authentication.name or hasRole('HR')")
 	public LeaveRequest request(String employee, LocalDate from, LocalDate to) {
 		LeaveRequest leaveRequest = LeaveRequest.builder()
 				.employee(employee)
@@ -24,26 +29,31 @@ public class LeaveRequestService {
 		return repo.save(leaveRequest);
 	}
 
+	@RolesAllowed("HR")
 	public Optional<LeaveRequest> approve(UUID id) {
 		Optional<LeaveRequest> found = repo.findById(id);
 		found.ifPresent(lr -> lr.setStatus(Status.APPROVED));
 		return found;
 	}
 
+	@RolesAllowed("HR")
 	public Optional<LeaveRequest> deny(UUID id) {
 		Optional<LeaveRequest> found = repo.findById(id);
 		found.ifPresent(lr -> lr.setStatus(Status.DENIED));
 		return found;
 	}
 
+	@PostAuthorize("returnObject.orElse(null)?.employee == authentication.name or hasRole('HR')")
 	public Optional<LeaveRequest> retrieve(UUID id) {
 		return repo.findById(id);
 	}
 
+	@PreAuthorize("#employee == authentication.name or hasRole('HR')")
 	public List<LeaveRequest> retrieveFor(String employee) {
 		return repo.findByEmployee(employee);
 	}
 
+	@RolesAllowed("HR")
 	public List<LeaveRequest> retrieveAll() {
 		return repo.findAll();
 	}
