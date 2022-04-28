da41d11cbc9e0c0602d30e6c27727196b60f60cb
Tim te Beek <tim.te.beek@jdriven.com>
2022-04-28 21:52:29 +0200

    Upgrade to Keycloak 18

--- spring-cloud-gateway-oidc-tokenrelay/travel-spring-cloud-gateway/src/main/java/com/jdriven/gateway/TravelGatewayApplication.java
+++ spring-cloud-gateway-oidc-tokenrelay/travel-spring-cloud-gateway/src/main/java/com/jdriven/gateway/TravelGatewayApplication.java
@@ -5,9 +5,10 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,10 +35,11 @@ public class TravelGatewayApplication {
 	@ResponseBody
 	public Map<String, Object> index(
 			@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-			Authentication auth) {
+			@AuthenticationPrincipal(errorOnInvalidType = true) OidcUser user) {
 		Map<String, Object> model = new HashMap<>();
 		model.put("clientName", authorizedClient.getClientRegistration().getClientName());
-		model.put("authName", auth.getName());
+		model.put("userName", user.getName());
+		model.put("userAttributes", user.getAttributes());
 		return model;
 	}
 
