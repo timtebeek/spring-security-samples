cfdf87aefb68b040e17a3dfe525ccc5b01826e56
Tim te Beek <tim.te.beek@jdriven.com>
2022-05-20 10:05:35 +0200

    Add now missing `@EnableWebSecurity`

--- adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/SecurityConfig.java
+++ adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.*;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @ConditionalOnMissingBean(JwtDecoder.class)
+@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
