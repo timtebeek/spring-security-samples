b2b20066935ba3813224b2d70eb33660c132fa39
Tim te Beek <tim.te.beek@jdriven.com>
2022-06-19 23:46:12 +0200

    Drop JwtDecoder MockBean from LeaveRequestServiceTest; add more tests

--- adding-spring-security/leaveapp-complete/src/test/java/com/jdriven/leaverequest/LeaveRequestServiceTest.java
+++ adding-spring-security/leaveapp-complete/src/test/java/com/jdriven/leaverequest/LeaveRequestServiceTest.java
@@ -1,15 +1,15 @@
 package com.jdriven.leaverequest;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import static com.jdriven.leaverequest.LeaveRequest.Status.APPROVED;
@@ -22,34 +22,81 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class LeaveRequestServiceTest {
 
-	@MockBean
-	private JwtDecoder jwtDecoder;
-
 	@SpyBean
 	private LeaveRequestRepository repository;
 
 	@Autowired
 	private LeaveRequestService service;
 
+	@BeforeEach
+	void beforeEach() {
+		repository.clear();
+	}
+
 	@Nested
+	@WithMockUser(username = "Alice")
 	class AuthorizeUser {
 
 		@Test
-		@WithMockUser("Alice")
 		void testRequest() {
-			LeaveRequest leaveRequest = service.request("Alice", of(2022, 11, 30), of(2022, 12, 3));
+			LeaveRequest leaveRequest = service.request("Alice", of(2022, 11, 30), of(2022, 12, 03));
 			verify(repository).save(leaveRequest);
+		}
+
+		@Test
+		void testRetrieveById() {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
+			Optional<LeaveRequest> retrieved = service.retrieve(saved.getId());
+			verify(repository).findById(saved.getId());
+			assertThat(retrieved).isPresent();
+			assertThat(retrieved).get().isSameAs(saved);
+		}
+
+		@Test
+		void testRetrieveForEmployee() {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
+			List<LeaveRequest> retrieved = service.retrieveFor("Alice");
+			verify(repository).findByEmployee("Alice");
+			assertThat(retrieved).containsExactly(saved);
 		}
 
 	}
 
 	@Nested
+	@WithMockUser(roles = "HR")
 	class AuthorizeRole {
 
 		@Test
-		@WithMockUser(roles = "HR")
+		void testRequest() {
+			LeaveRequest leaveRequest = service.request("Alice", of(2022, 11, 30), of(2022, 12, 03));
+			verify(repository).save(leaveRequest);
+		}
+
+		@Test
+		void testRetrieveById() {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
+			Optional<LeaveRequest> retrieved = service.retrieve(saved.getId());
+			verify(repository).findById(saved.getId());
+			assertThat(retrieved).isPresent();
+			assertThat(retrieved).get().isSameAs(saved);
+		}
+
+		@Test
+		void testRetrieveForEmployee() {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
+			List<LeaveRequest> retrieved = service.retrieveFor("Alice");
+			verify(repository).findByEmployee("Alice");
+			assertThat(retrieved).containsExactly(saved);
+		}
+
+		@Test
 		void testApprove() {
-			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 3), PENDING));
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
 			Optional<LeaveRequest> approved = service.approve(saved.getId());
 			verify(repository).findById(saved.getId());
 			assertThat(approved).isPresent();
@@ -58,14 +105,23 @@ class LeaveRequestServiceTest {
 		}
 
 		@Test
-		@WithMockUser(roles = "HR")
 		void testDeny() {
-			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 3), PENDING));
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
 			Optional<LeaveRequest> denied = service.deny(saved.getId());
 			verify(repository).findById(saved.getId());
 			assertThat(denied).isPresent();
 			assertThat(denied).get().isSameAs(saved);
 			assertThat(denied.get().getStatus()).isSameAs(DENIED);
+		}
+
+		@Test
+		void testRetrieveAll() {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2022, 11, 30), of(2022, 12, 03), PENDING));
+			List<LeaveRequest> denied = service.retrieveAll();
+			verify(repository).findAll();
+			assertThat(denied).containsExactly(saved);
 		}
 
 	}
