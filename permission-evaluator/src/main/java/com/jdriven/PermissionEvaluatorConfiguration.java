f10ebe6133fac38f7628b39cf4f8cf9135f65fac
Tim te Beek <timtebeek@gmail.com>
2022-07-19 15:12:32 +0200

    Adopt EnableMethodSecurity for custom PermissionEvaluator sample #115 (#118)
    * Adopt EnableMethodSecurity for custom PermissionEvaluator sample #115
    
    * Spotless apply
    
    * Add a section on Configure method security for Permission evaluator
    
    * Expand the link text
    
    * Fix imports

--- permission-evaluator/src/main/java/com/jdriven/PermissionEvaluatorConfiguration.java
+++ permission-evaluator/src/main/java/com/jdriven/PermissionEvaluatorConfiguration.java
@@ -1,10 +1,31 @@
 package com.jdriven;
 
+import com.jdriven.permission.CustomPermissionEvaluator;
+
+import org.springframework.aop.Advisor;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
+import org.springframework.security.authorization.method.PreAuthorizeAuthorizationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class PermissionEvaluatorConfiguration {
-	// Annotations only
+@EnableMethodSecurity(prePostEnabled = false)
+class PermissionEvaluatorConfiguration {
+
+	@Bean
+	@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+	Advisor preAuthorizeAuthorizationMethodInterceptor(CustomPermissionEvaluator customPermissionEvaluator) {
+		DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+		expressionHandler.setPermissionEvaluator(customPermissionEvaluator);
+
+		PreAuthorizeAuthorizationManager authorizationManager = new PreAuthorizeAuthorizationManager();
+		authorizationManager.setExpressionHandler(expressionHandler);
+
+		return AuthorizationManagerBeforeMethodInterceptor.preAuthorize(authorizationManager);
+	}
+
 }
