c039b70c7bcca596e757d0d1ef59f4fd85d2f06c
Cheizoo, W.F. (Willem) <willem.cheizoo@devolksbank.nl>
2023-03-24 14:50:14 +0100

    Run spotless

--- adding-spring-security/leaveapp-complete/src/test/java/com/jdriven/leaverequest/LeaveRequestServiceTest.java
+++ adding-spring-security/leaveapp-complete/src/test/java/com/jdriven/leaverequest/LeaveRequestServiceTest.java
@@ -1,16 +1,9 @@
 package com.jdriven.leaverequest;
 
-import static com.jdriven.leaverequest.LeaveRequest.Status.APPROVED;
-import static com.jdriven.leaverequest.LeaveRequest.Status.DENIED;
-import static com.jdriven.leaverequest.LeaveRequest.Status.PENDING;
-import static java.time.LocalDate.of;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,8 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import static com.jdriven.leaverequest.LeaveRequest.Status.APPROVED;
+import static com.jdriven.leaverequest.LeaveRequest.Status.DENIED;
+import static com.jdriven.leaverequest.LeaveRequest.Status.PENDING;
+import static java.time.LocalDate.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class LeaveRequestServiceTest {
