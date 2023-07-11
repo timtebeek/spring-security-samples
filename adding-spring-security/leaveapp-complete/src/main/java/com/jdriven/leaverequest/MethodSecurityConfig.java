b14c87837e315209f25613576d6b446057c0decc
Willem Cheizoo <willem.cheizoo@jdriven.com>
2023-07-11 11:36:42 +0200

    Add @Configuration to class with @EnableMethodSecurity

--- adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/MethodSecurityConfig.java
+++ adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/MethodSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.jdriven.leaverequest;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@Configuration
 @EnableMethodSecurity(jsr250Enabled = true)
 class MethodSecurityConfig {
 }
