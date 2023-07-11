e3708725701725086e1160446135a14e4b3665d0
Willem Cheizoo <hetzijzo@users.noreply.github.com>
2023-07-11 12:23:35 +0200

    Add configuration annotation to @Enable* annotated classes (#162)
    * Add @Configuration to class with @EnableMethodSecurity
    
    * Add some more @Configuration to classes with @Enable* annotations.

--- adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/WebSecurityConfig.java
+++ adding-spring-security/leaveapp-complete/src/main/java/com/jdriven/leaverequest/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.jdriven.leaverequest;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,68 +8,68 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @ConditionalOnWebApplication
 class WebSecurityConfig {
 
+	private static final MappedJwtClaimSetConverter MAPPED_JWT_CLAIM_CONVERTER = MappedJwtClaimSetConverter.withDefaults(Map.of());
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
-				.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-				.oauth2ResourceServer(oauth2 -> oauth2
-						.jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakRealmRoleConverter())))
-				.build();
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+			.oauth2ResourceServer(oauth2 -> oauth2
+				.jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakRealmRoleConverter())))
+			.build();
 	}
 
 	@Bean
 	@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.jwt.issuer-uri")
 	JwtDecoder jwtDecoderByIssuerUri(OAuth2ResourceServerProperties properties) {
-		String issuerUri = properties.getJwt().getIssuerUri();
-		NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
-		// Use preferred_username from claims as authentication name, instead of UUID subject
-		jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
+		final var issuerUri = properties.getJwt().getIssuerUri();
+		final var jwtDecoder = JwtDecoders.<NimbusJwtDecoder>fromIssuerLocation(issuerUri);
+		jwtDecoder.setClaimSetConverter(userNameSubClaimAdapter());
 		return jwtDecoder;
 	}
 
-}
-
-// As per: https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html#oauth2resourceserver-jwt-authorization-extraction
-class KeycloakRealmRoleConverter implements Converter<Jwt, JwtAuthenticationToken> {
-
-	@Override
+	/**
+	 * As per: <a href="https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html#oauth2resourceserver-jwt-authorization-extraction">...</a>
+	 */
 	@SuppressWarnings("unchecked")
-	public JwtAuthenticationToken convert(Jwt jwt) {
-		Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("realm_access", Collections.emptyMap());
-		List<String> roles = (List<String>) realmAccess.getOrDefault("roles", Collections.emptyList());
-		List<GrantedAuthority> authorities = roles.stream()
+	static Converter<Jwt, JwtAuthenticationToken> keycloakRealmRoleConverter() {
+		return jwt -> {
+			final var realmAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("realm_access", Map.of());
+			final var roles = (List<String>) realmAccess.getOrDefault("roles", List.of());
+			final var authorities = roles.stream()
 				.map(roleName -> "ROLE_" + roleName)
 				.map(SimpleGrantedAuthority::new)
 				.collect(Collectors.toList());
-		return new JwtAuthenticationToken(jwt, authorities);
+			return new JwtAuthenticationToken(jwt, authorities);
+		};
 	}
 
-}
-
-//As per: https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html#oauth2resourceserver-jwt-claimsetmapping
-class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
-
-	private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
-
-	@Override
-	public Map<String, Object> convert(Map<String, Object> claims) {
-		Map<String, Object> convertedClaims = this.delegate.convert(claims);
-		String username = (String) convertedClaims.get("preferred_username");
-		convertedClaims.put("sub", username);
-		return convertedClaims;
+	/**
+	 * Use preferred_username from claims as authentication name, instead of UUID subject.
+	 * As per: <a href="https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html#oauth2resourceserver-jwt-claimsetmapping">...</a>
+	 *
+	 * @return Converter
+	 */
+	static Converter<Map<String, Object>, Map<String, Object>> userNameSubClaimAdapter() {
+		return claims -> {
+			final var convertedClaims = MAPPED_JWT_CLAIM_CONVERTER.convert(claims);
+			final var username = (String) convertedClaims.get("preferred_username");
+			convertedClaims.put("sub", username);
+			return convertedClaims;
+		};
 	}
-
 }
--- gateway/dummy-service/src/main/java/com/jdriven/SecurityConfig.java
+++ gateway/dummy-service/src/main/java/com/jdriven/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.jdriven;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
